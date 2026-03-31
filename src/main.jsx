274023caa8bfa75ec51d3c176f60c34be90b089d
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-03-31 15:44:57 +0545

    Appended test url for new pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { TablesProvider } from './context/TablesContext';
+import { OrderProvider } from "./context/OrderContext";
+import { AutoFetchProvider   } from "./context/AutoFetchContext";
+import App from './App.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TablesProvider>
+    <OrderProvider>
+      <AutoFetchProvider>
+        <App />
+      </AutoFetchProvider>
+    </OrderProvider>
+    </TablesProvider>
   </StrictMode>,
 )
