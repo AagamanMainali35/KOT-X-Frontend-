a037d0fea916678dfc5835a3c9979c9c36e657cf
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-03-31 15:47:58 +0545

    Created new routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-// src/App.jsx
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Test from './pages/log';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
