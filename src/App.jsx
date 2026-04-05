95f239aed9f1f55ce821d3b894fa7cdd486e2729
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-04-05 21:28:10 +0545

    Url file for billing and admin dashbaord

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Test from './pages/menu';
 import AdminDashboard from "./pages/AdminDashboard"
+import MenuManagement from "./pages/MenuManagement"
+import Billing from "./pages/Billing"
 function App() {
   return (
+
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Order" element={<Test />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/menu" element={<MenuManagement />} />
+        <Route path="/billing" element={<Billing />} />
       </Routes>
     </Router>
   );
